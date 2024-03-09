a1c1e8379e3bb69141a6a31f02b2e43bc717d310
vitorlana45 <vitoresmerio00@gmail.com>
2024-03-08 23:32:52 -0300

    CityController and CityService crated and successful testing

---
+++ src/main/java/com/devsuperior/demo/controllers/CityController.java
@@ -0,0 +1,35 @@
+package com.devsuperior.demo.controllers;
+
+import com.devsuperior.demo.dto.CityDTO;
+import com.devsuperior.demo.services.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/cities")
+public class CityController {
+
+    @Autowired
+    private CityService service;
+
+    @GetMapping
+    public ResponseEntity<List<CityDTO>> findAll() {
+        List<CityDTO> CityList = service.findAll();
+
+        return ResponseEntity.ok().body(CityList);
+    }
+
+    @PostMapping
+    public ResponseEntity<CityDTO> insert(@RequestBody CityDTO cityDTO) {
+        cityDTO = service.insert(cityDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(cityDTO.getId()).toUri();
+        return ResponseEntity.created(uri).body(cityDTO);
+    }
+
+
+}
---
+++ src/main/java/com/devsuperior/demo/services/CityService.java
@@ -0,0 +1,39 @@
+package com.devsuperior.demo.services;
+
+import com.devsuperior.demo.dto.CityDTO;
+import com.devsuperior.demo.entities.City;
+import com.devsuperior.demo.repositories.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CityService {
+
+    @Autowired
+    private CityRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<CityDTO> findAll() {
+        List<City> dto = repository.findAll(Sort.by("name"));
+        return dto.stream().map(CityList -> new CityDTO(CityList)).toList();
+    }
+
+    @Transactional
+    public CityDTO insert(CityDTO cityDTO) {
+        City city = convertToCityDTO(cityDTO);
+        City dto = repository.save(city);
+        return new CityDTO(dto);
+    }
+
+
+    private City convertToCityDTO(CityDTO cityDTO) {
+        City city = new City();
+        city.setName(cityDTO.getName());
+        return city;
+    }
+
+}
