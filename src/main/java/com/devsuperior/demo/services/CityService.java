b2a7133057df6ec27b7f1c7c50110dbfa4ceba3b
vitorlana45 <vitoresmerio00@gmail.com>
2024-03-08 23:32:52 -0300

    CityController and CityService crated and successful testing

---
+++ src/main/java/com/devsuperior/demo/services/CityService.java
@@ -0,0 +1,40 @@
+package com.devsuperior.demo.services;
+
+import com.devsuperior.demo.dto.CityDTO;
+import com.devsuperior.demo.entities.City;
+import com.devsuperior.demo.repositories.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CityService {
+
+    @Autowired
+    private CityRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<CityDTO> findAll() {
+        List<City> dto = repository.findAll(Sort.by("name"));
+        return dto.stream().map(CityList -> new CityDTO(CityList)).toList();
+    }
+
+    @Transactional
+    public CityDTO insert(CityDTO cityDTO) {
+        City city = convertToCity(cityDTO);
+        City createdCity = repository.save(city);
+        return new CityDTO(createdCity);
+    }
+
+
+    private City convertToCity(CityDTO cityDTO) {
+        City city = new City();
+        city.setName(cityDTO.getName());
+        return city;
+    }
+
+
+}
